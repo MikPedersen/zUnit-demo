f9a9b2d89e81d7829768a7ae9e55724ec751dec8
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2019-10-08 11:50:28 +0200

    Initial commit

---
+++ src/TestSuite.java
@@ -0,0 +1,10 @@
+public abstract class TestSuite {
+    public static void runTestSuite(){
+        //TODO?
+    }
+    protected abstract void testList();
+    public void assertEquals(int expected, int actual){
+        //TODO?
+
+    }
+}
---
+++ test/TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass extends TestSuite {
+    @Override
+    protected void testList() {
+        testThat();
+        testThis();
+    }
+    public void testThis(){
+        //TODO?
+    }
+    public void testThat(){
+        //TODO?
+    }
+}
